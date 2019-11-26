1bec40b36b306dd881dad2930bea98db29a706de
Miłosz Chodkowski <mchodkowski@MacBook-Pro-Miosz.local>
2019-11-26 21:18:15 +0100

    Final

--- lab2/Sequence.py
+++ lab2/Sequence.py
@@ -1,39 +1,41 @@
 #!/usr/bin/python3
 '''Module written as an extra exercise for AMU's Python course.
 __author__ = Milosz Chodkowski PUT
-___field__ = Bioinformatics 
+___field__ = Bioinformatics
 ___version___ = 1.0'''
 
+
 class Sequence:
-	def __init__(self, seq='..(..)..()..'):
-		self.seq = seq
+    def __init__(self, seq='..(..)..()..'):
+        self.seq = seq
 
-	def validate(self, seq=None):
-		'''Return the boolean value of dot bracket validation.
-		>>> S.validate('..()..')
-		True'''
-		seq = self.seq if seq is None else seq
-		OPEN_PAR, CLOSED_PAR = '(', ')'
-		pars_map, valid_chars = {'(': ')'}, {'(', ')', '.'}
-		stack, pars = [], []
+    def validate(self, seq=None):
+        '''Return the boolean value of dot bracket validation.
+        >>> S.validate('..()..')
+        True'''
+        seq = self.seq if seq is None else seq
+        OPEN_PAR, CLOSED_PAR = '(', ')'
+        pars_map, valid_chars = {'(': ')'}, {'(', ')', '.'}
+        stack, pars = [], []
 
-		for char in seq:
-			# If there is any other char than parentheses or dot
-			if char not in valid_chars:
-				print('\'{}\' is not valid char in dot bracket format!'.format(char))
-				return False
-			if char == OPEN_PAR or char == CLOSED_PAR:
-				pars.append(char)
-				
-		for par in pars:
-			if par in pars_map:
-				stack.append(pars_map[par])
-			# If stack empty or last bracket not valid
-			elif not stack or par != stack.pop():
-				return False
-		return not stack
-		
+        for char in seq:
+            # If there is any other char than parentheses or dot
+            if char not in valid_chars:
+                print('\'{}\' not valid in dot bracket format!'.format(char))
+                return False
+            if char == OPEN_PAR or char == CLOSED_PAR:
+                pars.append(char)
+
+        for par in pars:
+            if par in pars_map:
+                stack.append(pars_map[par])
+            # If stack empty or last bracket not valid
+            elif not stack or par != stack.pop():
+                return False
+        return not stack
+
+
 if __name__ == '__main__':
-	S = Sequence()
-	print(S.validate())
-	print(S.validate('()))((()'))
+    S = Sequence()
+    print(S.validate())
+    print(S.validate('()))((()'))
--- lab3/lab3_extra.py
+++ lab3/lab3_extra.py
@@ -5,13 +5,17 @@ __author__ = Milosz Chodkowski PUT
 __field__ = Bioinformatics
 __version__ = 1.0
 '''
+
+
 class GeneFiles:
     def __init__(self, files_count=10, folder='data'):
         self.percents, self.names = [], []
         self.valid_bases = {'a', 't', 'c', 'g', 'u'}
-        self.file_names = ['{}/seq{}.genbank.txt'.format(folder, i) for i in range(1, files_count + 1)] # build list of file names
-    
-    def getPercentage(self):    
+        # build list of file names
+        self.file_names = ['{}/seq{}.genbank.txt'.format(folder, i)
+                           for i in range(1, files_count + 1)]
+
+    def getPercentage(self):
         for file_name in self.file_names:
             with open(file_name, 'r') as fh:
                 gc_bases, base_count = 0, 0
@@ -27,3 +31,7 @@ class GeneFiles:
         for name, percent in zip(self.names, self.percents):
             print('NAME:{} G/C PERCENTAGE: {:.2f} '.format(name, percent*100))
 
+
+if __name__ == '__main__':
+    G = GeneFiles()
+    G.getPercentage()
