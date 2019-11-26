d15682294d8e3b2c2f43753e23a5634b929357ce
Miłosz Chodkowski <mchodkowski@MacBook-Pro-Miosz.local>
2019-11-26 21:54:12 +0100

    Final

--- lab3/lab3_extra.py
+++ lab3/lab3_extra.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+import os
 '''
-Script opens 10 files in 'data' folder, then count's G/C percentage for every file'
+Script opens 10 files in 'data' folder.
+Then count's G/C percentage for every file.
 __author__ = Milosz Chodkowski PUT
 __field__ = Bioinformatics
 __version__ = 1.0
@@ -9,27 +11,27 @@ __version__ = 1.0
 
 class GeneFiles:
     def __init__(self, files_count=10, folder='data'):
+        self.THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
         self.percents, self.names = [], []
         self.valid_bases = {'a', 't', 'c', 'g', 'u'}
-        # build list of file names
-        self.file_names = ['{}/seq{}.genbank.txt'.format(folder, i)
-                           for i in range(1, files_count + 1)]
+        self.file_names = ['{}/{}/seq{}.genbank.txt'.format(
+            self.THIS_FOLDER, folder, i) for i in range(1, files_count + 1)]
 
     def getPercentage(self):
         for file_name in self.file_names:
-            with open(file_name, 'r') as fh:
+            with open(file_name, 'r') as my_file:
                 gc_bases, base_count = 0, 0
-                self.names.append(fh.readline().split()[1])
-                oh = fh.read().split('ORIGIN')[1]
+                self.names.append(my_file.readline().split()[1])
+                oh = my_file.read().split('ORIGIN')[1]
                 for base in oh:
                     if base in self.valid_bases:
                         base_count += 1
                         if base == 'g' or base == 'c':
                             gc_bases += 1
-                self.percents.append(gc_bases/base_count)
-            fh.close()
+                self.percents.append(gc_bases / base_count)
+            my_file.close()
         for name, percent in zip(self.names, self.percents):
-            print('NAME:{} G/C PERCENTAGE: {:.2f} '.format(name, percent*100))
+            print('NAME:{} G/C PERCENTAGE: {:.2f} '.format(name, percent * 100))
 
 
 if __name__ == '__main__':
