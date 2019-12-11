30325990de51ccea634d990fd72590048be9054a
777moneymaker <adeeeq77@gmail.com>
2019-12-11 10:56:24 +0100

    lab5

--- lab5/lab5_extra.py
+++ lab5/lab5_extra.py
@@ -6,39 +6,42 @@ from time import sleep
 __author__ = 'Milosz Chodkowski PUT'
 
 LETTERS = string.ascii_uppercase + ' '
+DEFAULT_SENTENCE = 'METHINKS IT IS LIKE A WEASEL'
 
-def hammingDist(sequence):
-	def_seq = 'METHINKS IT IS LIKE A WEASEL'
-	count = 0
-	for val1, val2 in zip(sequence, def_seq):
-		if val1 != val2:
-			count += 1
-	return (count, sequence)
 
-def generateSequence():
-	val = ''.join(random.choice(LETTERS) for i in range(28))
-	return val
+def hammingDist(sentence):
+    count = 0
+    for char1, char2 in zip(sentence, DEFAULT_SENTENCE):
+        if char1 != char2:
+            count += 1
+    return (count, sentence)
+
+
+def generateSentence():
+    new_sentence = ''.join(random.choice(LETTERS) for i in range(28))
+    return new_sentence
+
 
 def solve():
-	generations = 0
-	best_dist, best_solution = len(LETTERS), None
+    generation = 0
+    best_dist, best_solution = len(LETTERS), LETTERS
 
-	while best_dist > 0:
-		val = generateSequence() if not generations else best_solution
-		sequences, new_l = [val for i in range(100)], list()
-		for i in range(len(sequences)):
-			word = list(sequences[i])
-			word[random.randint(0, len(word) - 1)] = random.choice(LETTERS)
-			new_l.append(''.join(word))
-			solutions = map(hammingDist, new_l)
-			for sol in solutions:
-				if sol[0] < best_dist:
-					best_dist = sol[0]
-					best_solution = sol[1]
-		sleep(0.1)
-		print(generations + 1, best_solution)
-		generations += 1
+    while best_dist > 0:
+        sentence = generateSentence() if not generation else best_solution
+        sentences, new_l = [sentence for i in range(100)], list()
+        for word in sentences:
+            word = list(word)
+            word[random.randint(0, len(word) - 1)] = random.choice(LETTERS)
+            new_l.append(''.join(word))
+            solutions = map(hammingDist, new_l)
+            for sol in solutions:
+                if sol[0] < best_dist:
+                    best_dist = sol[0]
+                    best_solution = sol[1]
+        sleep(0.1)
+        print(generation + 1, best_solution)
+        generation += 1
+
 
 if __name__ == '__main__':
-	solve()
-			
+    solve()
