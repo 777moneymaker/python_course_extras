d09d1de5154f6de9d53bab010c7a45867f249744
777moneymaker <adeeeq77@gmail.com>
2019-12-15 11:19:33 +0100

    lab5

--- lab5/lab5_extra.py
+++ lab5/lab5_extra.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
 import string
 import random
-from time import sleep
 
 __author__ = 'Milosz Chodkowski PUT'
 
 LETTERS = string.ascii_uppercase + ' ' + '.'
-DEFAULT_SENTENCE = 'METHINKS IT IS LIKE A WEASEL.'
+TARGET_SENTENCE = 'METHINKS IT IS LIKE A WEASEL'
 
 
-def hammingDist(sentence):
+def hammingDistance(sentence):
     count = 0
-    for char1, char2 in zip(sentence, DEFAULT_SENTENCE):
+    for char1, char2 in zip(sentence, TARGET_SENTENCE):
         if char1 != char2:
             count += 1
-    return (count, sentence)
+    return tuple([count, sentence])
 
 
-def generateSentence():
-    new_sentence = ''.join(random.choice(LETTERS) for i in range(28))
+def generateRandomSentence():
+    new_sentence = ''.join(random.choice(LETTERS) for i in range(len(TARGET_SENTENCE)))
     return new_sentence
 
 
@@ -27,18 +26,18 @@ def solve():
     best_dist, best_solution = len(LETTERS), LETTERS
 
     while best_dist > 0:
-        sentence = generateSentence() if not generation else best_solution
-        sentences, new_l = [sentence for i in range(100)], list()
-        for word in sentences:
-            word = list(word)
-            word[random.randint(0, len(word) - 1)] = random.choice(LETTERS)
-            new_l.append(''.join(word))
-            solutions = map(hammingDist, new_l)
-            for sol in solutions:
-                if sol[0] < best_dist:
-                    best_dist = sol[0]
-                    best_solution = sol[1]
-        sleep(0.1)
+        random_sentence = generateRandomSentence() if not generation else best_solution
+        new_l, sentences = list(), [random_sentence for i in range(100)]
+        for mark in sentences:
+            mark = list(mark)
+            mark[random.randint(0, len(mark) - 1)] = random.choice(LETTERS)
+            new_l.append(''.join(mark))
+
+        solutions = map(hammingDistance, new_l)
+        for sol in solutions:
+            if sol[0] < best_dist:
+                best_dist = sol[0]
+                best_solution = sol[1]
         print(generation + 1, best_solution)
         generation += 1
 
