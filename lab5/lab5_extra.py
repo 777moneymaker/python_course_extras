2703f833a344f15e179cbd7050df08ef2e5a7915
777moneymaker <adeeeq77@gmail.com>
2019-12-24 14:33:51 +0100

    upgrades

--- lab5/lab5_extra.py
+++ lab5/lab5_extra.py
@@ -9,26 +9,20 @@ LETTERS = string.ascii_uppercase + ' '
 TARGET_SENTENCE = 'METHINKS IT IS LIKE A WEASEL'
 
 def hammingDistance(sentence):
-    count = 0
-    for char1, char2 in zip(sentence, TARGET_SENTENCE):
-        if char1 != char2:
-            count += 1
-    return tuple([count, sentence])
+    return sum(c1 != c2 for c1, c2 in zip(sentence, TARGET_SENTENCE)), sentence
 
 
 def generateRandomSentence():
-    new_sentence = ''.join(random.choice(LETTERS) for i in range(len(TARGET_SENTENCE)))
-    return new_sentence
+    return ''.join(random.choice(LETTERS) for i in range(len(TARGET_SENTENCE)))
 
 
 def solve():
-    first_genre = True
+    iteration = 0
     best_dist, best_solution = len(LETTERS) + 1, LETTERS # len == 27 + 1
-    best_solutions = list()
 
     while best_dist > 0:
-        random_sentence = generateRandomSentence() if first_genre else best_solution
-        new_l, sentences = list(), [random_sentence for i in range(100)]
+        random_sentence = generateRandomSentence() if not iteration else best_solution
+        new_l, sentences = list(), [random_sentence] * 100
         for mark in sentences:
             mark = list(mark)
             mark[random.randint(0, len(mark) - 1)] = random.choice(LETTERS)
@@ -36,19 +30,10 @@ def solve():
         solutions = map(hammingDistance, new_l)
         for sol in solutions:
             if sol[0] < best_dist:
-                best_dist = sol[0]
-                best_solution = sol[1]
-                first_genre = False
-                best_solutions.append(best_solution)
-    return best_solutions
-
-
-def main():
-    results = solve()
-    for i, result in enumerate(results, start=1):
-        sleep(0.1)
-        print(i, result)
+                best_dist, best_solution = sol[0], sol[1] 
+        iteration += 1
+        print(iteration, best_solution)
 
 
 if __name__ == '__main__':
-    main()
+    solve()
